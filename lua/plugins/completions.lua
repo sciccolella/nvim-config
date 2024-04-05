471a3dcdae78334639cb9d42a9e631dae229bc03
Simone Ciccolella <ciccolella.simone@gmail.com>
2024-04-05 16:22:44 +0200

    update completion with (almost) super TAB

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -1,3 +1,9 @@
+local has_words_before = function()
+  unpack = unpack or table.unpack
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
 local kind_icons = {
   Text = "",
   Method = "󰆧",
@@ -42,12 +48,14 @@ return {
   {
     "hrsh7th/nvim-cmp",
     config = function()
+      local luasnip = require("luasnip")
       local cmp = require("cmp")
       require("luasnip.loaders.from_vscode").lazy_load()
       cmp.setup({
         snippet = {
           expand = function(args)
-            require("luasnip").lsp_expand(args.body)
+            -- require("luasnip").lsp_expand(args.body)
+            luasnip.lsp_expand(args.body)
           end,
         },
         window = {
@@ -78,6 +86,32 @@ return {
           ["<C-Space>"] = cmp.mapping.complete(),
           ["<C-e>"] = cmp.mapping.abort(),
           ["<CR>"] = cmp.mapping.confirm({ select = true }),
+
+          ["<Tab>"] = cmp.mapping(function(fallback)
+            -- if cmp.visible() then
+            --   cmp.select_next_item()
+            -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
+            -- that way you will only jump inside the snippet region
+            -- elseif luasnip.expand_or_jumpable() then
+            if luasnip.expand_or_jumpable() then
+              luasnip.expand_or_jump()
+            elseif has_words_before() then
+              cmp.complete()
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+
+          ["<S-Tab>"] = cmp.mapping(function(fallback)
+            -- if cmp.visible() then
+            --   cmp.select_prev_item()
+            -- elseif luasnip.jumpable(-1) then
+            if luasnip.jumpable(-1) then
+              luasnip.jump(-1)
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
         }),
         sources = cmp.config.sources({
           { name = "nvim_lsp" },
