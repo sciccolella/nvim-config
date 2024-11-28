2f6d00ec12a9306c20666f4bf0360d41cdefbb77
Simone Ciccolella <simone.ciccolella@unimib.it>
2024-11-28 11:26:58 +0100

    new init

--- init.lua
+++ init.lua
@@ -1,5 +1,5 @@
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 vim.g.have_nerd_font = true
 vim.opt.number = true
 vim.opt.relativenumber = true
@@ -9,13 +9,14 @@ vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 
+vim.keymap.set("n", "<leader><leader>", "<C-^>")
 vim.opt.mouse = ""
 
 vim.opt.undofile = true    -- Save undo history
 vim.opt.signcolumn = "yes" -- Keep signcolumn on by default
 
-vim.opt.cursorline = true -- Show which line your cursor is on
-vim.opt.scrolloff = 10    -- Minimal number of screen lines to keep above and below the cursor.
+vim.opt.cursorline = true  -- Show which line your cursor is on
+vim.opt.scrolloff = 10     -- Minimal number of screen lines to keep above and below the cursor.
 
 -- Enable break indent
 vim.opt.breakindent = true
@@ -25,7 +26,7 @@ vim.opt.ignorecase = true
 vim.opt.smartcase = true
 
 -- Keep signcolumn on by default
-vim.opt.signcolumn = 'yes'
+vim.opt.signcolumn = "yes"
 
 -- Decrease update time
 vim.opt.updatetime = 250
@@ -35,16 +36,16 @@ vim.opt.timeoutlen = 300
 --  See `:help 'list'`
 --  and `:help 'listchars'`
 vim.opt.list = true
-vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
 -- Preview substitutions live, as you type!
-vim.opt.inccommand = 'split'
+vim.opt.inccommand = "split"
 
 -- Clear highlights on search when pressing <Esc> in normal mode
 --  See `:help hlsearch`
-vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 
 -- Diagnostic keymaps
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
 
 -- Keybinds to make split navigation easier.
 --  Use CTRL+<hjkl> to switch between windows
@@ -61,21 +62,35 @@ vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper win
 -- Highlight when yanking (copying) text
 --  Try it with `yap` in normal mode
 --  See `:help vim.highlight.on_yank()`
-vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+vim.api.nvim_create_autocmd("TextYankPost", {
+  desc = "Highlight when yanking (copying) text",
+  group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
   callback = function()
     vim.highlight.on_yank()
   end,
 })
 
 
--- STATUS
---  %<%f:%y%{v:lua.vim.lsp.buf_get_clients()}%=\ %m%r%h\ %-15(%l,%c%V%)%b'
+-- STATUSLINE
 -- set statusline=%<%f%=\ [%1*%M%*%n%R%H]\ %-19(%3l,%02c%03V%)%O'%02b'
 -- vim.o.statusline = [[%<%f %h%m%r %y%=%{v:register} %-14.(%l,%c%V%) %P]]
 -- vim.o.statusline = [[%<%f:%y%{v:lua.vim.lsp.buf_get_clients()}%= %m%r%h %-15(%l,%c%V%)%b']]
-vim.o.statusline = [[%<%f:%y%{g:sl_lsp}%= %m%r%h %-15(%l,%c%V%)%b']]
+-- vim.o.statusline = [[%<%f:%y%{g:sl_lsp}%= %m%r%h %-15(%l,%c%V%)%b']]
+function _G.sl_lsp()
+  local bufnr = vim.api.nvim_get_current_buf()
+  local atch_clients = vim.lsp.get_clients({ bufnr = bufnr, })
+  if #atch_clients == 0 then
+    return ""
+  end
+  local it = vim.iter(atch_clients)
+  it:map(function(clt)
+    return clt.name
+  end)
+  local lsps = it:totable()
+  return "[" .. table.concat(lsps, ",") .. "]"
+end
+
+vim.o.statusline = [[%<%f:%y%{%v:lua._G.sl_lsp()%}%= %m%r%h %-15(%l,%c%V%)%b']]
 
 -- THEME
 require("sictm_hi")
@@ -89,7 +104,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
   if vim.v.shell_error ~= 0 then
     vim.api.nvim_echo({
       { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
+      { out,                            "WarningMsg" },
       { "\nPress any key to exit..." },
     }, true, {})
     vim.fn.getchar()
@@ -99,6 +114,18 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
+  { -- Highlight, edit, and navigate code
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    main = "nvim-treesitter.configs", -- Sets main module to use for opts
+    -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
+    opts = {
+      ensure_installed = { "bash", "c", "diff", "lua", "luadoc", "markdown", "markdown_inline", "query", "vim", "vimdoc", "python" },
+      auto_install = true, -- Autoinstall languages that are not installed
+      highlight = { enable = true, },
+      indent = { enable = true, disable = {} },
+    },
+  },
   {
     "folke/which-key.nvim",
     event = "VeryLazy",
@@ -116,31 +143,30 @@ require("lazy").setup({
     },
   },
   {
-    'nvim-telescope/telescope.nvim', 
-    branch = '0.1.x',
-    dependencies = { 'nvim-lua/plenary.nvim' },
+    "nvim-telescope/telescope.nvim",
+    branch = "0.1.x",
+    dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
       require("telescope").setup {
       }
 
       local builtin = require("telescope.builtin")
-      vim.keymap.set('n', '<C-p>', builtin.find_files, {})
-      vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+      vim.keymap.set("n", "<C-p>", builtin.find_files, {})
+      vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
 
       -- This is from kickstart.nvim
       -- See `:help telescope.builtin`
-      vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
-      vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
-      vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
-      vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
-      vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
-      vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
-      vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
-      vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
-      vim.keymap.set('n', '<leader>sm', builtin.marks, { desc = '[S]earch [m]arks' })
-      vim.keymap.set('n', '<leader>sb', builtin.buffers, { desc = '[S]earch [b]uffer' })
-      vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
-      vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
+      vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "[S]earch [H]elp" })
+      vim.keymap.set("n", "<leader>sk", builtin.keymaps, { desc = "[S]earch [K]eymaps" })
+      vim.keymap.set("n", "<leader>sf", builtin.find_files, { desc = "[S]earch [F]iles" })
+      vim.keymap.set("n", "<leader>ss", builtin.builtin, { desc = "[S]earch [S]elect Telescope" })
+      vim.keymap.set("n", "<leader>sw", builtin.grep_string, { desc = "[S]earch current [W]ord" })
+      vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch by [G]rep" })
+      vim.keymap.set("n", "<leader>sd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
+      vim.keymap.set("n", "<leader>sr", builtin.resume, { desc = "[S]earch [R]esume" })
+      vim.keymap.set("n", "<leader>sm", builtin.marks, { desc = "[S]earch [m]arks" })
+      vim.keymap.set("n", "<leader>sb", builtin.buffers, { desc = "[S]earch [b]uffer" })
+      vim.keymap.set("n", "<leader>s.", builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
     end
   },
   {
@@ -148,12 +174,132 @@ require("lazy").setup({
     dependencies = { "nvim-lua/plenary.nvim" },
     opts = {
       signs = false,
-
       highlight = {
         keyword = "bg",
       },
     },
   },
-  -- LSP
+  { "neovim/nvim-lspconfig", },
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      {
+        "L3MON4D3/LuaSnip",
+        dependencies = {
+          -- "rafamadriz/friendly-snippets",
+        },
+      },
+      "saadparwaiz1/cmp_luasnip",
+      -- 'hrsh7th/cmp-path',
+      -- 'hrsh7th/cmp-buffer',
+      "hrsh7th/cmp-nvim-lsp",
+    },
+    config = function()
+      -- local has_words_before = function()
+      --   unpack = unpack or table.unpack
+      --   local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+      --   return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+      -- end
+      --
 
+      local cmp = require "cmp"
+      local luasnip = require "luasnip"
+      luasnip.config.setup {}
+
+      cmp.setup {
+        snippet = {
+          expand = function(args)
+            luasnip.lsp_expand(args.body)
+          end,
+        },
+        window = {
+          -- completion = cmp.config.window.bordered(),
+          documentation = cmp.config.window.bordered(),
+        },
+        formatting = {
+          fields = { "kind", "abbr", "menu" },
+          format = function(entry, vim_item)
+            vim_item.kind = string.format("%s", vim_item.kind)
+            vim_item.menu = ({
+              -- buffer = "[Buffer]",
+              path = "[Path]",
+              nvim_lsp = "[LSP]",
+              luasnip = "[LuaSnip]",
+            })[entry.source.name]
+            return vim_item
+          end,
+        },
+        mapping = cmp.mapping.preset.insert {
+          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+          ["<C-f>"] = cmp.mapping.scroll_docs(4),
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<C-e>"] = cmp.mapping.abort(),
+          ["<C-p>"] = cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Insert },
+          ["<C-n>"] = cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Insert },
+          ["<C-y>"] = cmp.mapping.confirm({ select = true }),
+
+        },
+        sources = cmp.config.sources({
+          { name = "nvim_lsp" },
+          { name = "luasnip" },
+        }, {
+          { name = "buffer" },
+          { name = "path" },
+        }),
+
+      }
+    end
+  },
 })
+
+
+
+vim.api.nvim_create_autocmd("LspAttach", {
+  desc = "LSP actions",
+  callback = function(event)
+    -- local opts = { buffer = event.buf }
+
+    vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+    vim.keymap.set("n", "<leader>lK", vim.lsp.buf.hover, { desc = "Display hover information" })
+    vim.keymap.set("n", "<leader>lgd", vim.lsp.buf.definition, { desc = "[g]o to [d]efinition" })
+    vim.keymap.set("n", "<leader>lgD", vim.lsp.buf.declaration, { desc = "[g]o to [D]eclaration" })
+    vim.keymap.set("n", "<leader>lgi", vim.lsp.buf.implementation, { desc = "[g]o to [i]mplementation" })
+    vim.keymap.set({ "n", "v" }, "<leader>la", vim.lsp.buf.code_action, { desc = "Code [a]ction" })
+    vim.keymap.set("n", "<leader>lr", vim.lsp.buf.rename, { desc = "[r]ename token" })
+    vim.keymap.set("n", "<leader>lf", vim.lsp.buf.format, { desc = "[f]ormat buffer" })
+  end,
+})
+
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
+require("lspconfig").lua_ls.setup({
+  capabilities = capabilities,
+  settings = {
+    Lua = {
+      runtime = { version = "Lua 5.1" },
+      diagnostics = {
+        globals = { "vim" },
+      },
+      format = {
+        enable = true,
+        -- Put format options here
+        -- NOTE: the value should be STRING!!
+        defaultConfig = {
+          indent_style = "space",
+          indent_size = "2",
+          indent_width = "2",
+          quote_style = "double",
+        }
+      },
+    },
+  },
+})
+
+require("lspconfig").clangd.setup({
+  capabilities = capabilities,
+  cmd = {
+    "clangd",
+    "--offset-encoding=utf-16",
+  },
+})
+
+require("lspconfig").gopls.setup({})
